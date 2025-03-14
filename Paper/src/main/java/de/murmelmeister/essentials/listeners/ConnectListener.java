c70a5d82f6bc9e0f0310d418af4f3ee1be96f259
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-03-14 21:48:05 +0100

    feat: add player quit event handling to update commands

--- Paper/src/main/java/de/murmelmeister/essentials/listeners/ConnectListener.java
+++ Paper/src/main/java/de/murmelmeister/essentials/listeners/ConnectListener.java
@@ -5,6 +5,7 @@ import de.murmelmeister.essentials.manager.ListenerManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public final class ConnectListener extends ListenerManager {
     public ConnectListener(MurmelEssentials instance) {
@@ -13,6 +14,12 @@ public final class ConnectListener extends ListenerManager {
 
     @EventHandler
     public void handlePlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.updateCommands();
+    }
+
+    @EventHandler
+    public void handlePlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         player.updateCommands();
     }
