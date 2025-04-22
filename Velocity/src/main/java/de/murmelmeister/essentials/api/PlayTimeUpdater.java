d46cdccf174c0e08b4aff7b6eee8fd7305ace796
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-04-22 13:52:20 +0200

    Refactor PlayTimeUpdater to improve task management and logging

--- Velocity/src/main/java/de/murmelmeister/essentials/api/PlayTimeUpdater.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/api/PlayTimeUpdater.java
@@ -1,23 +1,41 @@
 package de.murmelmeister.essentials.api;
 
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
+import com.velocitypowered.api.scheduler.ScheduledTask;
+import com.velocitypowered.api.scheduler.TaskStatus;
 import de.murmelmeister.essentials.MurmelEssentials;
-import de.murmelmeister.murmelapi.playtime.PlayTime;
+import de.murmelmeister.murmelapi.time.PlayTime;
 import de.murmelmeister.murmelapi.user.User;
+import org.slf4j.Logger;
 
 import java.util.concurrent.TimeUnit;
 
 public final class PlayTimeUpdater {
-    public static void startTimer(ProxyServer server, MurmelEssentials instance) {
-        var user = instance.getUser();
-        var playTime = instance.getPlayTime();
-        server.getScheduler().buildTask(instance, () -> updateTimer(server, user, playTime)).repeat(1L, TimeUnit.SECONDS).schedule();
+    private static ScheduledTask task;
+
+    private static void cancelExistingTask() {
+        if (task != null && task.status() != TaskStatus.CANCELLED)
+            task.cancel();
     }
 
-    private static void updateTimer(ProxyServer server, User user, PlayTime playTime) {
-        for (var player : server.getAllPlayers()) {
-            var userId = user.getId(player.getUniqueId());
-            playTime.timer(userId);
+    private static void updateTimer(ProxyServer server, Logger logger, User user, PlayTime playTime) {
+        for (Player player : server.getAllPlayers()) {
+            if (!user.existsUser(player.getUniqueId())) {
+                logger.warn("Player {} doesn't exist in users table.", player.getUsername());
+                continue;
+            }
+
+            int userId = user.getId(player.getUniqueId());
+            playTime.addTime(userId);
         }
+    }
+
+    public static void startTimer(MurmelEssentials plugin, ProxyServer server, Logger logger) {
+        cancelExistingTask();
+        User user = plugin.getUser();
+        PlayTime playTime = plugin.getPlayTime();
+        task = server.getScheduler().buildTask(playTime, () -> updateTimer(server, logger, user, playTime))
+                .repeat(1L, TimeUnit.SECONDS).schedule();
     }
 }
