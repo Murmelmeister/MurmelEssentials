2553ad902cfc659e373805f374a1de3853264971
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-04-23 22:55:39 +0200

    Add login and disconnect event handling in PermissionListener

--- Velocity/src/main/java/de/murmelmeister/essentials/listeners/PermissionListener.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/listeners/PermissionListener.java
@@ -2,15 +2,20 @@ package de.murmelmeister.essentials.listeners;
 
 import com.velocitypowered.api.event.Continuation;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.DisconnectEvent;
+import com.velocitypowered.api.event.connection.LoginEvent;
 import com.velocitypowered.api.event.permission.PermissionsSetupEvent;
 import com.velocitypowered.api.proxy.Player;
 import de.murmelmeister.essentials.api.CustomPermission;
 import de.murmelmeister.murmelapi.permission.Permission;
+import de.murmelmeister.murmelapi.user.User;
 
 public final class PermissionListener {
+    private final User user;
     private final Permission permission;
 
-    public PermissionListener(Permission permission) {
+    public PermissionListener(User user, Permission permission) {
+        this.user = user;
         this.permission = permission;
     }
 
@@ -27,5 +32,19 @@ public final class PermissionListener {
         } finally {
             continuation.resume();
         }
+    }
+
+    @Subscribe
+    public void handleLogin(LoginEvent event) {
+        Player player = event.getPlayer();
+        int userId = user.getId(player.getUniqueId());
+        permission.preloadAsync(userId);
+    }
+
+    @Subscribe
+    public void handleDisconnect(DisconnectEvent event) {
+        Player player = event.getPlayer();
+        int userId = user.getId(player.getUniqueId());
+        permission.invalidate(userId);
     }
 }
