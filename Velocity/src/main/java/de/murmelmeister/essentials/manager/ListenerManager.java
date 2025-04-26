3d9c268701288f2d4d4a813089a3eda137bf64f9
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-04-27 00:21:56 +0200

    Update ListenerManager to use RefreshMessageListener for cache invalidation

--- Velocity/src/main/java/de/murmelmeister/essentials/manager/ListenerManager.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/manager/ListenerManager.java
@@ -2,7 +2,7 @@ package de.murmelmeister.essentials.manager;
 
 import com.velocitypowered.api.proxy.ProxyServer;
 import de.murmelmeister.essentials.MurmelEssentials;
-import de.murmelmeister.essentials.listeners.CacheInvalidateListener;
+import de.murmelmeister.essentials.listeners.RefreshMessageListener;
 import de.murmelmeister.essentials.listeners.ConnectionListener;
 import de.murmelmeister.essentials.listeners.PermissionListener;
 import de.murmelmeister.murmelapi.user.User;
@@ -10,7 +10,7 @@ import de.murmelmeister.murmelapi.user.User;
 public final class ListenerManager {
     public static void register(MurmelEssentials plugin, ProxyServer server) {
         User user = plugin.getUser();
-        addListener(plugin, server, new CacheInvalidateListener(plugin));
+        addListener(plugin, server, new RefreshMessageListener(plugin, server));
         addListener(plugin, server, new PermissionListener(user, plugin.getPermission()));
         addListener(plugin, server, new ConnectionListener(user, plugin.getGroup(), plugin.getPlayTime(), plugin.getActiveSession()));
     }
