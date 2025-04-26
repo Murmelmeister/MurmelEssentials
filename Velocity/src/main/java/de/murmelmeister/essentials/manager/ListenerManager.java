55a899f5a61a446609cbcb8322202b71aa21ffb7
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-04-26 22:42:59 +0200

    Register CacheInvalidateListener and refactor listener initialization in ListenerManager

--- Velocity/src/main/java/de/murmelmeister/essentials/manager/ListenerManager.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/manager/ListenerManager.java
@@ -2,13 +2,17 @@ package de.murmelmeister.essentials.manager;
 
 import com.velocitypowered.api.proxy.ProxyServer;
 import de.murmelmeister.essentials.MurmelEssentials;
+import de.murmelmeister.essentials.listeners.CacheInvalidateListener;
 import de.murmelmeister.essentials.listeners.ConnectionListener;
 import de.murmelmeister.essentials.listeners.PermissionListener;
+import de.murmelmeister.murmelapi.user.User;
 
 public final class ListenerManager {
     public static void register(MurmelEssentials plugin, ProxyServer server) {
-        addListener(plugin, server, new PermissionListener(plugin.getPermission()));
-        addListener(plugin, server, new ConnectionListener(plugin.getUser(), plugin.getGroup(), plugin.getPlayTime(), plugin.getActiveSession()));
+        User user = plugin.getUser();
+        addListener(plugin, server, new CacheInvalidateListener(plugin));
+        addListener(plugin, server, new PermissionListener(user, plugin.getPermission()));
+        addListener(plugin, server, new ConnectionListener(user, plugin.getGroup(), plugin.getPlayTime(), plugin.getActiveSession()));
     }
 
     private static void addListener(MurmelEssentials plugin, ProxyServer server, Object clazz) {
