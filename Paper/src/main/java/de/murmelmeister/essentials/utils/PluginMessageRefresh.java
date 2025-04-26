1a1f2613cce1366f429025087ddceafd8553a29c
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-04-26 22:41:59 +0200

    Refactor plugin message handling to use a buffer for cache name in PluginMessageRefresh

--- Paper/src/main/java/de/murmelmeister/essentials/utils/PluginMessageRefresh.java
+++ Paper/src/main/java/de/murmelmeister/essentials/utils/PluginMessageRefresh.java
@@ -1,16 +1,20 @@
 package de.murmelmeister.essentials.utils;
 
+import de.murmelmeister.essentials.MurmelEssentials;
+import de.murmelmeister.murmelapi.utils.BufferUtils;
 import de.murmelmeister.murmelapi.utils.update.RefreshUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.jetbrains.annotations.NotNull;
 
+import java.nio.ByteBuffer;
+
 public final class PluginMessageRefresh implements PluginMessageListener {
     @Override
     public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, byte @NotNull [] message) {
-        if (channel.equals("permission:refresh")) {
-            var msg = new String(message);
-            if (msg.equals("refresh")) RefreshUtil.markAsRefreshed();
-        }
+        if (!channel.equals(MurmelEssentials.CHANNEL)) return;
+        ByteBuffer buffer = ByteBuffer.wrap(message);
+        String cacheName = BufferUtils.readUTF(buffer);
+        RefreshUtil.markAsRefreshed(cacheName);
     }
 }
