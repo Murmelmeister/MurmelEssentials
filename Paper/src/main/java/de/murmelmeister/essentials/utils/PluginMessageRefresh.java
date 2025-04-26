4959d42d6c0c0dcf1ccfaf318852885eab50ff88
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-04-27 00:23:00 +0200

    Refactor PluginMessageRefresh to use BufferUtils.decodeUTF for message handling

--- Paper/src/main/java/de/murmelmeister/essentials/utils/PluginMessageRefresh.java
+++ Paper/src/main/java/de/murmelmeister/essentials/utils/PluginMessageRefresh.java
@@ -7,14 +7,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 import org.jetbrains.annotations.NotNull;
 
-import java.nio.ByteBuffer;
-
 public final class PluginMessageRefresh implements PluginMessageListener {
     @Override
     public void onPluginMessageReceived(@NotNull String channel, @NotNull Player player, byte @NotNull [] message) {
         if (!channel.equals(MurmelEssentials.CHANNEL)) return;
-        ByteBuffer buffer = ByteBuffer.wrap(message);
-        String cacheName = BufferUtils.readUTF(buffer);
+        String cacheName = BufferUtils.decodeUTF(message);
         RefreshUtil.markAsRefreshed(cacheName);
     }
 }
