4e0f68c19a56a555d421e15782375bdbea45b4e2
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-05-01 22:58:53 +0200

    Add CommandBrigadier interface for Brigadier command creation

---
+++ Velocity/src/main/java/de/murmelmeister/essentials/manager/CommandBrigadier.java
@@ -0,0 +1,7 @@
+package de.murmelmeister.essentials.manager;
+
+import com.velocitypowered.api.command.BrigadierCommand;
+
+public interface CommandBrigadier {
+    BrigadierCommand createCommand();
+}
