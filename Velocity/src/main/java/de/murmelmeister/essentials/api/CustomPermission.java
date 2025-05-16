eb494f6c00714912b7f2af8a86e739e096b2b5bf
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-05-16 20:58:25 +0200

    Update permission refresh interval and mark permissions as refreshed

--- Velocity/src/main/java/de/murmelmeister/essentials/api/CustomPermission.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/api/CustomPermission.java
@@ -11,6 +11,8 @@ import com.velocitypowered.api.scheduler.ScheduledTask;
 import com.velocitypowered.api.scheduler.TaskStatus;
 import de.murmelmeister.essentials.MurmelEssentials;
 import de.murmelmeister.murmelapi.permission.Permission;
+import de.murmelmeister.murmelapi.utils.update.RefreshType;
+import de.murmelmeister.murmelapi.utils.update.RefreshUtil;
 
 import java.util.concurrent.TimeUnit;
 
@@ -39,7 +41,11 @@ public final class CustomPermission implements PermissionProvider, PermissionFun
     public static void updatePermission(MurmelEssentials plugin, ProxyServer server) {
         if (task != null && task.status() != TaskStatus.CANCELLED)
             task.cancel();
-        task = server.getScheduler().buildTask(plugin, () -> plugin.getPermission().loadExpired())
-                .repeat(10, TimeUnit.SECONDS).schedule();
+        task = server.getScheduler().buildTask(plugin, () -> {
+                    int updatedRows = plugin.getPermission().loadExpired();
+                    if (updatedRows > 0)
+                        RefreshUtil.markAsRefreshed(RefreshType.PERMISSIONS); // TODO: And group cache / parent cache etc.
+                })
+                .repeat(1, TimeUnit.SECONDS).schedule();
     }
 }
