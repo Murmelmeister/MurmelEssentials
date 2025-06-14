47f33472b26edb3ca9ed6e718b2987aed9d443c1
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-06-14 15:30:44 +0200

    Add Config class for database connection management

---
+++ Velocity/src/main/java/de/murmelmeister/essentials/configurations/Config.java
@@ -0,0 +1,34 @@
+package de.murmelmeister.essentials.configurations;
+
+import de.murmelmeister.essentials.MurmelEssentials;
+import de.murmelmeister.essentials.utils.ConfigValue;
+import de.murmelmeister.murmelapi.MurmelAPI;
+import de.murmelmeister.murmelapi.configuration.YamlMurmel;
+
+public class Config {
+    private final YamlMurmel config = YamlMurmel.builder(MurmelEssentials.PLUGIN_PATH + "config.yaml").build();
+
+    public <T> T getValue(ConfigValue value, Class<T> type) {
+        return config.getValue(value.getPath(), type, type.cast(value.getFallback()));
+    }
+
+    public void connectToDatabase() {
+        String databaseName = getValue(ConfigValue.DB_DATABASE, String.class);
+        String dbDriver = getValue(ConfigValue.DB_DRIVER, String.class);
+        String dbHostname = getValue(ConfigValue.DB_HOSTNAME, String.class);
+        Integer dbPort = getValue(ConfigValue.DB_PORT, Integer.class);
+        String dbUsername = getValue(ConfigValue.DB_USERNAME, String.class);
+        String dbPassword = getValue(ConfigValue.DB_PASSWORD, String.class);
+        if (databaseName == null || dbDriver == null || dbHostname == null || dbPort == null || dbUsername == null || dbPassword == null ||
+            dbUsername.equalsIgnoreCase("<USERNAME>") || dbPassword.equalsIgnoreCase("<PASSWORD>"))
+            throw new IllegalArgumentException("Database configuration is incomplete or contains placeholder. Please check your config.yaml file.");
+
+        MurmelAPI.setDatabaseName(databaseName);
+        String url = String.format("jdbc:%s://%s:%s/%s", dbDriver, dbHostname, dbPort, databaseName);
+        MurmelAPI.connect(url, dbUsername, dbPassword);
+    }
+
+    public void disconnectFromDatabase() {
+        MurmelAPI.disconnect();
+    }
+}
