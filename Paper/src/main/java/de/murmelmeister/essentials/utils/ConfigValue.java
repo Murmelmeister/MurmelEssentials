8ac75242a01c3346f0a010229141b4f972c0ba2a
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-06-14 15:30:32 +0200

    Add ConfigValue enum for database configuration management

---
+++ Paper/src/main/java/de/murmelmeister/essentials/utils/ConfigValue.java
@@ -0,0 +1,28 @@
+package de.murmelmeister.essentials.utils;
+
+public enum ConfigValue {
+    DB_DRIVER("database.driver", "mysql"),
+    DB_DATABASE("database.name", "MurmelAPI"),
+    DB_HOSTNAME("database.hostname", "localhost"),
+    DB_PORT("database.port", 3306),
+    DB_USERNAME("database.username", "<USERNAME>"),
+    DB_PASSWORD("database.password", "<PASSWORD>"),
+    ;
+    public static final ConfigValue[] VALUES = values();
+
+    private final String path;
+    private final Object fallback;
+
+    ConfigValue(String path, Object fallback) {
+        this.path = path;
+        this.fallback = fallback;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Object getFallback() {
+        return fallback;
+    }
+}
