196cab572f33ca6ea0e8dd48dd1a11f6528c93f9
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-08-01 21:05:32 +0200

    Add RefreshListener to ListenerManager for handling refresh events

--- Paper/src/main/java/de/murmelmeister/essentials/manager/ListenerManager.java
+++ Paper/src/main/java/de/murmelmeister/essentials/manager/ListenerManager.java
@@ -3,24 +3,14 @@ package de.murmelmeister.essentials.manager;
 import de.murmelmeister.essentials.MurmelEssentials;
 import de.murmelmeister.essentials.listeners.CustomPermissionListener;
 import de.murmelmeister.essentials.listeners.PlayerChatListener;
-import de.murmelmeister.murmelapi.permission.Permission;
-import de.murmelmeister.murmelapi.user.User;
+import de.murmelmeister.essentials.listeners.RefreshListener;
 import org.bukkit.event.Listener;
 
 public class ListenerManager implements Listener {
-    protected final MurmelEssentials instance;
-    protected final User user;
-    protected final Permission permission;
-
-    public ListenerManager(MurmelEssentials instance) {
-        this.instance = instance;
-        this.user = instance.getUser();
-        this.permission = instance.getPermission();
-    }
-
     public static void register(MurmelEssentials instance) {
         addListener(instance, new CustomPermissionListener(instance));
         addListener(instance, new PlayerChatListener(instance));
+        addListener(instance, new RefreshListener());
     }
 
     private static void addListener(MurmelEssentials instance, Listener listener) {
