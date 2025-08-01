8e5d65c22969719817a203ad00c0d1d736618f9b
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-08-01 21:02:09 +0200

    Refactor LanguageCommand to remove user dependency, enhance language retrieval, and improve message handling

--- Velocity/src/main/java/de/murmelmeister/essentials/commands/LanguageCommand.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/commands/LanguageCommand.java
@@ -10,13 +10,20 @@ import de.murmelmeister.essentials.MurmelEssentials;
 import de.murmelmeister.essentials.manager.CommandManager;
 import de.murmelmeister.essentials.manager.command.CommandResult;
 import de.murmelmeister.murmelapi.language.Language;
+import de.murmelmeister.murmelapi.language.LanguageProvider;
+import de.murmelmeister.murmelapi.user.UserProvider;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
 import java.util.List;
 
 public final class LanguageCommand extends CommandManager {
+    private final LanguageProvider languageProvider;
+    private final UserProvider userProvider;
+
     public LanguageCommand(MurmelEssentials plugin) {
         super(plugin);
+        this.languageProvider = plugin.getLanguageProvider();
+        this.userProvider = plugin.getUserProvider();
     }
 
     @Override
@@ -24,7 +31,7 @@ public final class LanguageCommand extends CommandManager {
         LiteralCommandNode<CommandSource> node = BrigadierCommand.literalArgumentBuilder("language")
                 .requires(source -> source.hasPermission("murmel.command.language"))
                 .executes(context ->
-                        runWithTiming(context, (source, executorId) -> {
+                        runWithTiming(context, (source, executor) -> {
                             List<Language> languages = languageProvider.getLanguages();
 
                             if (languages.isEmpty()) {
@@ -32,7 +39,7 @@ public final class LanguageCommand extends CommandManager {
                                 return CommandResult.of(-2);
                             }
 
-                            int languageId = user.getLanguage(executorId);
+                            int languageId = executor.languageId();
                             Language language = languageProvider.get(languageId);
 
                             if (language == null) {
@@ -42,9 +49,9 @@ public final class LanguageCommand extends CommandManager {
 
                             sendMessage(source, "<#999999>===- %s:", languages.size() == 1 ? "Language" : "Languages");
                             languages.forEach(lang -> {
-                                boolean isCurrent = lang.getId() == languageId;
+                                boolean isCurrent = lang.id() == languageId;
                                 String hoverText = isCurrent ? "<#00cc88>Current" : "<#999999>Available";
-                                String langName = isCurrent ? "<#00cc88>" + lang.getName() : "<#999999>" + lang.getName();
+                                String langName = isCurrent ? "<#00cc88>" + lang.name() : "<#999999>" + lang.name();
                                 sendMessage(source, "<#999999>- <#00cc88><hover:show_text:'%s'>%s</hover>", hoverText, langName);
                             });
                             return CommandResult.of(Command.SINGLE_SUCCESS);
@@ -53,13 +60,13 @@ public final class LanguageCommand extends CommandManager {
                 .then(BrigadierCommand.requiredArgumentBuilder("language", StringArgumentType.word())
                         .suggests((context, builder) -> {
                             languageProvider.getLanguages().forEach(lang ->
-                                    builder.suggest(lang.getName().toLowerCase(),
-                                            VelocityBrigadierMessage.tooltip(MiniMessage.miniMessage().deserialize("<#00cc88>" + lang.getName())))
+                                    builder.suggest(lang.name().toLowerCase(),
+                                            VelocityBrigadierMessage.tooltip(MiniMessage.miniMessage().deserialize("<#00cc88>" + lang.name())))
                             );
                             return builder.buildFuture();
                         })
                         .executes(context ->
-                                runWithTiming(context, (source, executorId) -> {
+                                runWithTiming(context, (source, executor) -> {
                                     String languageName = StringArgumentType.getString(context, "language");
                                     Language language = languageProvider.get(languageName);
 
@@ -68,8 +75,9 @@ public final class LanguageCommand extends CommandManager {
                                         return CommandResult.of(-2);
                                     }
 
-                                    user.setLanguage(executorId, language.getId());
-                                    sendMessage(source, "<#999999>Language set to <#00cc88>%s</#00cc88>.", language.getName());
+                                    userProvider.update(executor.id(), executor.username(), executor.firstLogin(),
+                                            executor.debugUser(), executor.debugEnabled(), language.id()); // Update the user in the database
+                                    sendMessage(source, "<#999999>Language set to <#00cc88>%s</#00cc88>.", language.name());
                                     return CommandResult.of(Command.SINGLE_SUCCESS);
                                 })
                         )
