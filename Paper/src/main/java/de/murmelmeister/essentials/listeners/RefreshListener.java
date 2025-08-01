0470941747bf983f30767c4c4a76e6c097cb550e
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-08-01 21:05:27 +0200

    Add RefreshListener to handle ReceiveRefreshEvent and send player messages (test)

---
+++ Paper/src/main/java/de/murmelmeister/essentials/listeners/RefreshListener.java
@@ -0,0 +1,19 @@
+package de.murmelmeister.essentials.listeners;
+
+import de.murmelmeister.essentials.events.ReceiveRefreshEvent;
+import de.murmelmeister.essentials.manager.ListenerManager;
+import de.murmelmeister.murmelapi.utils.update.RefreshType;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+
+public class RefreshListener extends ListenerManager {
+    @EventHandler
+    public void handleRefresh(ReceiveRefreshEvent event) {
+        String cache = event.getType();
+        if (cache.equalsIgnoreCase(RefreshType.SINGLE_USER_PLAY_TIME.getName())) return;
+        String key = event.getKey();
+        Player player = event.getPlayer();
+        player.sendMessage(MiniMessage.miniMessage().deserialize("<#8800cc>CacheName: " + cache + ", Key: " + key));
+    }
+}
