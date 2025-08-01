9c6cb0c6196b3cc6bbba0d5730a39da096782efb
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-08-01 21:00:18 +0200

    Refactor PermissionListener to remove user dependency and simplify constructor

--- Velocity/src/main/java/de/murmelmeister/essentials/listeners/PermissionListener.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/listeners/PermissionListener.java
@@ -2,21 +2,17 @@ package de.murmelmeister.essentials.listeners;
 
 import com.velocitypowered.api.event.Continuation;
 import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.connection.DisconnectEvent;
-import com.velocitypowered.api.event.connection.LoginEvent;
 import com.velocitypowered.api.event.permission.PermissionsSetupEvent;
 import com.velocitypowered.api.proxy.Player;
+import de.murmelmeister.essentials.MurmelEssentials;
 import de.murmelmeister.essentials.api.CustomPermission;
 import de.murmelmeister.murmelapi.permission.Permission;
-import de.murmelmeister.murmelapi.user.User;
 
 public final class PermissionListener {
-    private final User user;
     private final Permission permission;
 
-    public PermissionListener(User user, Permission permission) {
-        this.user = user;
-        this.permission = permission;
+    public PermissionListener(MurmelEssentials plugin) {
+        this.permission = plugin.getPermission();
     }
 
     @Subscribe
@@ -32,19 +28,5 @@ public final class PermissionListener {
         } finally {
             continuation.resume();
         }
-    }
-
-    @Subscribe
-    public void handleLogin(LoginEvent event) {
-        Player player = event.getPlayer();
-        int userId = user.getId(player.getUniqueId());
-        permission.preloadAsync(userId);
-    }
-
-    @Subscribe
-    public void handleDisconnect(DisconnectEvent event) {
-        Player player = event.getPlayer();
-        int userId = user.getId(player.getUniqueId());
-        permission.invalidate(userId);
     }
 }
