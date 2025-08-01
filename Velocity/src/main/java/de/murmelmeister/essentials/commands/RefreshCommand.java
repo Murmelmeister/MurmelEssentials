e9f66ed610ee7d992f0c8db63da4e0bf7af837eb
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-08-01 21:02:52 +0200

    Add RefreshCommand enhancements to support multiple cache reloads and user message sending

--- Velocity/src/main/java/de/murmelmeister/essentials/commands/RefreshCommand.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/commands/RefreshCommand.java
@@ -1,6 +1,7 @@
 package de.murmelmeister.essentials.commands;
 
 import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandSource;
@@ -20,25 +21,16 @@ public final class RefreshCommand extends CommandManager {
         LiteralCommandNode<CommandSource> node = BrigadierCommand.literalArgumentBuilder("refresh")
                 .requires(source -> source.hasPermission("murmel.command.refresh"))
                 .executes(context ->
-                        runWithTiming(context, (source, executorId) -> {
-                            RefreshUtil.globalRefresh();
+                        runWithTiming(context, (source, executor) -> {
+                            RefreshUtil.fireAll();
                             sendMessage(source, "<#00cc88>All caches refreshed.");
                             return CommandResult.of(Command.SINGLE_SUCCESS);
                         })
                 )
-                .then(BrigadierCommand.literalArgumentBuilder("permissions")
-                        .executes(context ->
-                                runWithTiming(context, (source, executorId) -> {
-                                    RefreshUtil.markAsRefreshed(RefreshType.PERMISSIONS);
-                                    sendMessage(source, "<#00cc88>All caches refreshed.");
-                                    return CommandResult.of(Command.SINGLE_SUCCESS);
-                                })
-                        )
-                )
                 .then(BrigadierCommand.literalArgumentBuilder("languages")
                         .executes(context ->
-                                runWithTiming(context, (source, executorId) -> {
-                                    RefreshUtil.markAsRefreshed(RefreshType.LANGUAGES);
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.LANGUAGES);
                                     sendMessage(source, "<#00cc88>Reloaded languages.");
                                     return CommandResult.of(Command.SINGLE_SUCCESS);
                                 })
@@ -46,11 +38,104 @@ public final class RefreshCommand extends CommandManager {
                 )
                 .then(BrigadierCommand.literalArgumentBuilder("messages")
                         .executes(context ->
-                                runWithTiming(context, (source, executorId) -> {
-                                    RefreshUtil.markAsRefreshed(RefreshType.MESSAGES);
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.MESSAGES);
                                     sendMessage(source, "<#00cc88>Reloaded messages.");
                                     return CommandResult.of(Command.SINGLE_SUCCESS);
                                 })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("punishment_reasons")
+                        .executes(context ->
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.PUNISHMENT_REASONS);
+                                    sendMessage(source, "<#00cc88>Reloaded punishment reasons.");
+                                    return CommandResult.of(Command.SINGLE_SUCCESS);
+                                })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("punishment_logs")
+                        .executes(context ->
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.PUNISHMENT_LOGS);
+                                    sendMessage(source, "<#00cc88>Reloaded punishment logs.");
+                                    return CommandResult.of(Command.SINGLE_SUCCESS);
+                                })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("punishment_users")
+                        .executes(context ->
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.PUNISHMENT_USERS);
+                                    sendMessage(source, "<#00cc88>Reloaded punishment users.");
+                                    return CommandResult.of(Command.SINGLE_SUCCESS);
+                                })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("punishment_ips")
+                        .executes(context ->
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.PUNISHMENT_IPS);
+                                    sendMessage(source, "<#00cc88>Reloaded punishment IPs.");
+                                    return CommandResult.of(Command.SINGLE_SUCCESS);
+                                })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("users")
+                        .executes(context ->
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.USERS);
+                                    sendMessage(source, "<#00cc88>Reloaded users.");
+                                    return CommandResult.of(Command.SINGLE_SUCCESS);
+                                })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("user_logins")
+                        .executes(context ->
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.USER_LOGINS);
+                                    sendMessage(source, "<#00cc88>Reloaded user logins.");
+                                    return CommandResult.of(Command.SINGLE_SUCCESS);
+                                })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("user_sessions")
+                        .executes(context ->
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.USER_SESSIONS);
+                                    sendMessage(source, "<#00cc88>Reloaded user sessions.");
+                                    return CommandResult.of(Command.SINGLE_SUCCESS);
+                                })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("user_play_times")
+                        .executes(context ->
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.USER_PLAY_TIMES);
+                                    sendMessage(source, "<#00cc88>Reloaded user play times.");
+                                    return CommandResult.of(Command.SINGLE_SUCCESS);
+                                })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("all")
+                        .executes(context ->
+                                runWithTiming(context, (source, executor) -> {
+                                    RefreshUtil.fireCache(RefreshType.ALL);
+                                    sendMessage(source, "<#00cc88>Reloaded all caches.");
+                                    return CommandResult.of(Command.SINGLE_SUCCESS);
+                                })
+                        )
+                )
+                .then(BrigadierCommand.literalArgumentBuilder("send")
+                        .then(BrigadierCommand.requiredArgumentBuilder("message", StringArgumentType.word())
+                                .executes(context ->
+                                        runWithTiming(context, (source, executor) -> {
+                                            String message = StringArgumentType.getString(context, "message");
+                                            RefreshUtil.fireCache(message);
+                                            sendMessage(source, "<#00cc88>Sent message: " + message);
+                                            return CommandResult.of(Command.SINGLE_SUCCESS);
+                                        })
+                                )
                         )
                 )
                 .build();
