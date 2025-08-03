c3be722dbb1d4ce513befe1bded253d404cabbd7
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-08-03 12:12:26 +0200

    Add ConnectionListener to ListenerManager for player connection events

--- Paper/src/main/java/de/murmelmeister/essentials/manager/ListenerManager.java
+++ Paper/src/main/java/de/murmelmeister/essentials/manager/ListenerManager.java
@@ -1,6 +1,7 @@
 package de.murmelmeister.essentials.manager;
 
 import de.murmelmeister.essentials.MurmelEssentials;
+import de.murmelmeister.essentials.listeners.ConnectionListener;
 import de.murmelmeister.essentials.listeners.CustomPermissionListener;
 import de.murmelmeister.essentials.listeners.PlayerChatListener;
 import de.murmelmeister.essentials.listeners.RefreshListener;
@@ -8,9 +9,10 @@ import org.bukkit.event.Listener;
 
 public class ListenerManager implements Listener {
     public static void register(MurmelEssentials instance) {
-        addListener(instance, new CustomPermissionListener(instance));
+        addListener(instance, new CustomPermissionListener(instance.getPermission()));
         addListener(instance, new PlayerChatListener(instance));
         addListener(instance, new RefreshListener());
+        addListener(instance, new ConnectionListener(instance.getRanks()));
     }
 
     private static void addListener(MurmelEssentials instance, Listener listener) {
