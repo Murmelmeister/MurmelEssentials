b4d5de7303ef07ea7505b2a3450452af29070dc9
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-08-03 12:12:19 +0200

    Add ConnectionListener to handle player join and quit events

---
+++ Paper/src/main/java/de/murmelmeister/essentials/listeners/ConnectionListener.java
@@ -0,0 +1,25 @@
+package de.murmelmeister.essentials.listeners;
+
+import de.murmelmeister.essentials.api.Ranks;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class ConnectionListener implements Listener {
+    private final Ranks ranks;
+
+    public ConnectionListener(Ranks ranks) {
+        this.ranks = ranks;
+    }
+
+    @EventHandler
+    public void handlePlayerJoin(PlayerJoinEvent event) {
+        ranks.getHasUpdated().set(true);
+    }
+
+    @EventHandler
+    public void handlePlayerQuit(PlayerQuitEvent event) {
+        ranks.getHasUpdated().set(true);
+    }
+}
