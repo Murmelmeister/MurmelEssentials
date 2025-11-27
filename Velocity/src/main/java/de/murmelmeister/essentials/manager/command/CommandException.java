a937a0393a31c1757287b9828c8aeea7d3a1a910
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2025-11-27 20:09:35 +0100

    feat: extend CommandException to support Message-based exceptions with TagResolver handling

--- Velocity/src/main/java/de/murmelmeister/essentials/manager/command/CommandException.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/manager/command/CommandException.java
@@ -1,15 +1,41 @@
 package de.murmelmeister.essentials.manager.command;
 
+import de.murmelmeister.essentials.messages.Message;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+
 public class CommandException extends RuntimeException {
+    private final Message messageKey;
+    private final TagResolver[] resolvers;
+
     public CommandException(String message) {
         super(message);
+        this.messageKey = null;
+        this.resolvers = null;
     }
 
     public CommandException(String message, Throwable cause) {
         super(message, cause);
+        this.messageKey = null;
+        this.resolvers = null;
     }
 
     public CommandException(Throwable cause) {
         super(cause);
+        this.messageKey = null;
+        this.resolvers = null;
+    }
+
+    public CommandException(Message messageKey, TagResolver... resolvers) {
+        super(messageKey.getTag());
+        this.messageKey = messageKey;
+        this.resolvers = resolvers;
+    }
+
+    public Message getMessageKey() {
+        return messageKey;
+    }
+
+    public TagResolver[] getResolvers() {
+        return resolvers;
     }
 }
