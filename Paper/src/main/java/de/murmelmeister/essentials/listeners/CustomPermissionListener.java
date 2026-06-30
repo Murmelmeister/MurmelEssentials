0fbc741f422ad5fe6caf07e005198e87d4d1dbb5
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-07-01 01:36:07 +0200

    refactor: improve permission handling in CustomPermissionListener and enhance error logging

--- Paper/src/main/java/de/murmelmeister/essentials/listeners/CustomPermissionListener.java
+++ Paper/src/main/java/de/murmelmeister/essentials/listeners/CustomPermissionListener.java
@@ -1,29 +1,40 @@
 package de.murmelmeister.essentials.listeners;
 
 import de.murmelmeister.essentials.api.CustomPermission;
-import de.murmelmeister.murmelapi.permission.Permission;
+import de.murmelmeister.murmelapi.permission.PermissionService;
+import de.murmelmeister.murmelapi.user.UserProvider;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.slf4j.Logger;
 
 import java.lang.reflect.Field;
 
 public final class CustomPermissionListener implements Listener {
-    private final Permission permission;
+    private final Logger logger;
+    private final UserProvider userProvider;
+    private final PermissionService permissionService;
 
-    public CustomPermissionListener(Permission permission) {
-        this.permission = permission;
+    public CustomPermissionListener(Logger logger, UserProvider userProvider, PermissionService permissionService) {
+        this.logger = logger;
+        this.userProvider = userProvider;
+        this.permissionService = permissionService;
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void handlePlayerLogin(PlayerJoinEvent event) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+    public void handlePlayerLogin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
-        Field field = Class.forName("org.bukkit.craftbukkit.entity.CraftHumanEntity").getDeclaredField("perm");
-        field.setAccessible(true);
-        field.set(player, new CustomPermission(player, permission));
-        field.setAccessible(false);
+        try {
+            Field field = Class.forName("org.bukkit.craftbukkit.entity.CraftHumanEntity").getDeclaredField("perm");
+            field.setAccessible(true);
+            field.set(player, new CustomPermission(player, userProvider, permissionService));
+            field.setAccessible(false);
+        } catch (NoSuchFieldException | SecurityException | ClassNotFoundException | IllegalArgumentException |
+                 IllegalAccessException e) {
+            logger.error("Could not set custom permission for player {}", player.getName(), e);
+        }
         // Maybe could work? -> player.getServer().getPluginManager().subscribeToDefaultPerms(false, new CustomPermission(player, permission));
     }
 }
