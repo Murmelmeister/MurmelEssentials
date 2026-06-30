d73106eb85be894075c124cfb581a2c892924ef2
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-07-01 01:54:03 +0200

    refactor: update CustomPermission to use UserProvider and PermissionService for permission checks

--- Paper/src/main/java/de/murmelmeister/essentials/api/CustomPermission.java
+++ Paper/src/main/java/de/murmelmeister/essentials/api/CustomPermission.java
@@ -1,22 +1,30 @@
 package de.murmelmeister.essentials.api;
 
-import de.murmelmeister.murmelapi.permission.Permission;
+import de.murmelmeister.murmelapi.permission.PermissionService;
+import de.murmelmeister.murmelapi.permission.PermissionTarget;
+import de.murmelmeister.murmelapi.user.User;
+import de.murmelmeister.murmelapi.user.UserProvider;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissibleBase;
 import org.jetbrains.annotations.NotNull;
 
 public final class CustomPermission extends PermissibleBase {
     private final Player player;
-    private final Permission permission;
+    private final UserProvider userProvider;
+    private final PermissionService permissionService;
 
-    public CustomPermission(Player player, Permission permission) {
+    public CustomPermission(Player player, UserProvider userProvider, PermissionService permissionService) {
         super(player);
         this.player = player;
-        this.permission = permission;
+        this.userProvider = userProvider;
+        this.permissionService = permissionService;
     }
 
     @Override
     public boolean hasPermission(@NotNull String perm) {
-        return this.permission.hasPermission(this.player.getUniqueId(), perm);
+        User user = userProvider.findByMojangId(player.getUniqueId()).orElse(null);
+        if (user == null) return false;
+
+        return permissionService.hasPermission(PermissionTarget.user(user.id()), perm);
     }
 }
