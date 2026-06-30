f9a653da5532e5196907301610b4dccfa49f01cc
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-07-01 01:55:01 +0200

    refactor: simplify RefreshListener by removing logger and enhancing user retrieval

--- Paper/src/main/java/de/murmelmeister/essentials/listeners/RefreshListener.java
+++ Paper/src/main/java/de/murmelmeister/essentials/listeners/RefreshListener.java
@@ -3,33 +3,28 @@ package de.murmelmeister.essentials.listeners;
 import de.murmelmeister.essentials.events.ReceiveRefreshEvent;
 import de.murmelmeister.murmelapi.user.User;
 import de.murmelmeister.murmelapi.user.UserProvider;
-import de.murmelmeister.murmelapi.utils.update.RefreshType;
-import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.slf4j.Logger;
 
 public final class RefreshListener implements Listener {
-    private final Logger logger;
     private final UserProvider userProvider;
 
-    public RefreshListener(Logger logger, UserProvider userProvider) {
-        this.logger = logger;
+    public RefreshListener(UserProvider userProvider) {
         this.userProvider = userProvider;
     }
 
     @EventHandler
     public void handleRefresh(ReceiveRefreshEvent event) {
         String cache = event.getType();
-        if (cache.equalsIgnoreCase(RefreshType.SINGLE_USER_PLAY_TIME.getName())) return;
         String key = event.getKey();
         Player player = event.getPlayer();
-        User user = userProvider.findByMojangId(player.getUniqueId());
+        User user = userProvider.findByMojangId(player.getUniqueId()).orElse(null);
         if (user == null) return;
         if (user.debugMode()) {
-            logger.info("Cache: {}, Key: {}", cache, key);
-            player.sendMessage(MiniMessage.miniMessage().deserialize("<#8800cc>CacheName: " + cache + ", Key: " + key));
+            player.sendRichMessage("<#8800cc>Cache - Type: <cache_type>, Key: <cache_key>",
+                    Placeholder.unparsed("cache_type", cache), Placeholder.unparsed("cache_key", key));
         }
     }
 }
