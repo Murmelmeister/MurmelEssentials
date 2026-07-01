74c75abf75691af2a3998728d51f68800cc1e0f9
Murmelmeister <49274952+Murmelmeister@users.noreply.github.com>
2026-07-01 12:09:23 +0200

    refactor: update TranslatorListener to use LanguageType and improve user retrieval

--- Velocity/src/main/java/de/murmelmeister/essentials/listeners/TranslatorListener.java
+++ Velocity/src/main/java/de/murmelmeister/essentials/listeners/TranslatorListener.java
@@ -4,13 +4,13 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.player.PlayerSettingsChangedEvent;
 import com.velocitypowered.api.proxy.player.PlayerSettings;
 import de.murmelmeister.essentials.MurmelEssentials;
-import de.murmelmeister.murmelapi.language.Language;
-import de.murmelmeister.murmelapi.language.LanguageProvider;
+import de.murmelmeister.murmelapi.language.LanguageType;
+import de.murmelmeister.murmelapi.language.LanguageTypeProvider;
 import de.murmelmeister.murmelapi.user.User;
 import de.murmelmeister.murmelapi.user.UserProvider;
 
 public class TranslatorListener {
-    private final LanguageProvider languageProvider;
+    private final LanguageTypeProvider languageProvider;
     private final UserProvider userProvider;
 
     public TranslatorListener(MurmelEssentials plugin) {
@@ -21,10 +21,10 @@ public class TranslatorListener {
     @Subscribe
     public void handlePlayerSettings(PlayerSettingsChangedEvent event) {
         PlayerSettings settings = event.getPlayerSettings();
-        Language language = languageProvider.get(settings.getLocale().toLanguageTag());
+        LanguageType language = languageProvider.findByCode(settings.getLocale().toLanguageTag()).orElse(null);
         if (language == null) return;
 
-        User user = userProvider.findByMojangId(event.getPlayer().getUniqueId());
+        User user = userProvider.findByMojangId(event.getPlayer().getUniqueId()).orElse(null);
         if (user == null) return;
 
         userProvider.update(user.id(), user.username(), user.firstLogin(),
